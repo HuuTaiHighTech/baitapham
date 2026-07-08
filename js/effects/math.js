259d3fead6fa8b60824d531724c338faff2d0646
tai <huutai.nguyen.dev@gmail.com>
2026-07-08 13:19:35 +0700

    update task 4

--- js/effects/math.js
+++ js/effects/math.js
@@ -1,23 +1,37 @@
-function calculateArea(length, width) {
-    const Area = length * width;
-    return `${Area}`;
+function calculateArea(length, width){
+    return length * width;
 }
-function calculatePerimeter(length, width) {
-    const Perimeter = (length + width) * 2;
-    return `${Perimeter}`;
-}
-export function initMath(){
-    const lengthEl = document.getElementById('Length-El');
-    const widthEl = document.getElementById('Width-El');
-    const btnEl = document.getElementById('btn-El');
-    const resultMathEl= document.getElementById('resultMath-El');
 
-    btnEl.addEventListener('click', () => {
-    const valuelength = Number(lengthEl.value);
-    const valuewidth = Number(widthEl.value);
-    const  area = calculateArea(valuelength, valuewidth);
-    const perimeter = calculatePerimeter(valuelength, valuewidth);
-   resultMathEl.textContent = `Diện tích: ${area} - Chu vi: ${perimeter}`;
-    
-    });
+function calculatePerimeter(length, width){
+    return (length + width)*2
+}
+function validatelogic(length, width){
+    if(isFinite(length) && isFinite(width)){
+        if(length >= 0 && width >= 0){
+            return true;
+        }else{
+            alert("Dữ liệu số không hợp lệ");
+            return false;
+        }
+    }else {
+        alert("Không được chữa ký tự");
+        return false;
+    }
+}
+
+export function mainRectangle(){
+    let btn = document.getElementById('btnele');
+
+   btn.addEventListener('click', () =>{
+    let length =parseFloat(document.getElementById('lengthele').value);
+    let width = parseFloat(document.getElementById('widthele').value);
+    let result = document.getElementById('resultMath');
+    if(validatelogic(length, width)){
+        let area = calculateArea(length, width);
+        let perimeter = calculatePerimeter(length, width);
+        result.textContent = "chu vi là" + perimeter + "Diện tích là " + area;
+        console.log("chu vi là " + perimeter + "\nDiện tích là " + area);
+    }
+   }) 
+   
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 import { initSalary } from "./effects/salary.js";
 import { mainUSD } from "./effects/usd.js";
 import { mainAVG } from "./effects/avg.js";
+import { mainRectangle } from "./effects/math.js";
 
-// import { initUSD } from "./effects/usd.js";
-// import { initMath } from "./effects/math.js";
 // import { initdigit } from "./effects/digit.js";
 
 // import { initStudent } from "./effects/student.js";
@@ -15,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     initSalary();
     mainUSD();
     mainAVG();
+    mainRectangle();
     // initUSD ();
     // initMath();
     // initdigit();
